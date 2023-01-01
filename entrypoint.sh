3c03f9e5a9e32f75be5ce72177ddad310202a0d0
ifeng <84112641+hiifeng@users.noreply.github.com>
2023-01-01 09:07:58 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,6 @@
 # Web Site:https://www.hicairo.com
 # Telegram:https://t.me/HiaiFeng
 
-sed -i "s/UUID/$UUID/g" /etc/v2ray/config.json
-sed -i "s/VMESS_WSPATH/$VMESS_WSPATH/g" /etc/v2ray/config.json
-sed -i "s/VLESS_WSPATH/$VLESS_WSPATH/g" /etc/v2ray/config.json
-sed -i "s/VMESS_WSPATH/$VMESS_WSPATH/g" /etc/nginx/nginx.conf
-sed -i "s/VLESS_WSPATH/$VLESS_WSPATH/g" /etc/nginx/nginx.conf
+sed -i "s/UUID/$UUID/g;s/VMESS_WSPATH/$VMESS_WSPATH/g;s/VLESS_WSPATH/$VLESS_WSPATH/g" /etc/v2ray/config.json
+sed -i "s/VMESS_WSPATH/$VMESS_WSPATH/g;s/VLESS_WSPATH/$VLESS_WSPATH/g" /etc/nginx/nginx.conf
 exec "$@"
