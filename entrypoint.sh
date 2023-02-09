bed4b89998dd5b0a593075108a44e79e0ac053dc
ifeng <84112641+hiifeng@users.noreply.github.com>
2023-02-09 10:12:07 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,3 +6,8 @@
 sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#$VMESS_WSPATH#g;s#VLESS_WSPATH#$VLESS_WSPATH#g" /etc/v2ray/config.json
 sed -i "s#VMESS_WSPATH#$VMESS_WSPATH#g;s#VLESS_WSPATH#$VLESS_WSPATH#g" /etc/nginx/nginx.conf
 # exec "$@"
+
+# 请删掉下一行最前面的 # 号，同时将这行代码中的域名替换成 Replit 为你项目分配的域名。
+while true ;do curl https://xx.xx.repl.co; sleep 300; done &
+
+/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
