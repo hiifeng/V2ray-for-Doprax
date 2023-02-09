353ae9feb60f6a9a448a7f372b123fa39293d1b1
ifeng <84112641+hiifeng@users.noreply.github.com>
2023-02-09 10:14:59 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,6 @@ sed -i "s#VMESS_WSPATH#$VMESS_WSPATH#g;s#VLESS_WSPATH#$VLESS_WSPATH#g" /etc/ngin
 # exec "$@"
 
 # 请删掉下一行最前面的 # 号，同时将这行代码中的域名替换成 Replit 为你项目分配的域名。
-while true ;do curl https://xx.xx.repl.co; sleep 300; done &
+#while true ;do curl https://xx.xx.repl.co; sleep 300; done &
 
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
